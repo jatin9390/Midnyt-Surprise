c1c31ad56b9be0f1016bbf9e7bc9ab2ef7cfae72
Jatin <jatinaj1111@gmail.com>
2026-01-14 22:07:02 +0530

    Fix local server startup (listen on port 3000)

--- api/index.js
+++ api/index.js
@@ -82,4 +82,12 @@ app.get('/', (req, res) => {
     res.send('Midnyt Surprise API (Vercel) 🌙');
 });
 
+const PORT = process.env.PORT || 3000;
+
+if (require.main === module) {
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
 module.exports = app;
